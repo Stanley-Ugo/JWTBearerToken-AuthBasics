0239bd56616635c09572a21dfb6d2c8ea992ed01
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-05-05 01:10:17 +0100

    Adding HomeController

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using AuthBasics.CustomPolicyProvider;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            
+        }
+
+    }
+}
