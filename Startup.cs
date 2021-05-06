6107f3e4f6acebc64214afcf28491e073606578c
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-05-06 23:58:38 +0100

    setting Up Authentication

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,14 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
@@ -17,8 +20,31 @@ namespace Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthentication("OAuth")
-                .AddJwtBearer("OAuth", config => { 
+                .AddJwtBearer("OAuth", config => 
+                {
+                    var secretBytes = Encoding.UTF8.GetBytes(Constants.Secret);
 
+                    var key = new SymmetricSecurityKey(secretBytes);
+
+                    config.Events = new JwtBearerEvents()
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            if (context.Request.Query.ContainsKey("access_token"))
+                            {
+                                context.Token = context.Request.Query["access_token"];
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
+
+                    config.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = Constants.Issuer,
+                        ValidAudience = Constants.Audience,
+                        IssuerSigningKey = key,
+                    };
                 });
 
             services.AddControllersWithViews();
@@ -41,7 +67,6 @@ namespace Server
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
-                endpoints.MapRazorPages();
             });
         }
     }
