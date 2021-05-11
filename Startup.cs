4eba7f66f1cb6b573774a5b165f25265ae2037fe
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-05-11 01:12:02 +0100

    adding the cookies service

--- Startup.cs
+++ Startup.cs
@@ -20,7 +20,7 @@ namespace Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthentication("OAuth")
-                .AddJwtBearer("OAuth", config => 
+                .AddJwtBearer("OAuth", config =>  
                 {
                     var secretBytes = Encoding.UTF8.GetBytes(Constants.Secret);
 
