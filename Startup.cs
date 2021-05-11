214e4ffc56ecb43e22ae3f51cc0a095372d86d41
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-05-11 01:10:48 +0100

    OAuth Configurations

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,7 @@ namespace Server
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthentication("OAuth")
